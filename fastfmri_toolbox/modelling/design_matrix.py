e24d2cb68a30d262ec77e2c8f989d754726912d4
Geoff <gnvngo@gmail.com>
2023-05-17 16:20:17 -0400

    run black

--- fastfmri_toolbox/modelling/design_matrix.py
+++ fastfmri_toolbox/modelling/design_matrix.py
@@ -8,16 +8,20 @@ import pandas as pd
 import nibabel as nib
 import itertools
 
+
 class DesignMatrixRegressors:
     def __init__(self):
         self.confounds_required = False
 
-    def get(self, confounds: Optional[pd.DataFrame] = None) -> Tuple[np.ndarray, List[str]]:
+    def get(
+        self, confounds: Optional[pd.DataFrame] = None
+    ) -> Tuple[np.ndarray, List[str]]:
         raise NotImplementedError
-    
+
+
 class FrequencyRegressors(DesignMatrixRegressors):
     def __init__(
-        self, 
+        self,
         search_frequencies: List[float],
         time_points: Tuple[float, float],
     ):
@@ -26,9 +30,10 @@ class FrequencyRegressors(DesignMatrixRegressors):
         self.search_frequencies = search_frequencies
         self.time_points = time_points
         self.n_tps = len(self.time_points)
-    
-    def get(self, confounds: Optional[pd.DataFrame] = None) -> Tuple[np.ndarray, List[str]]:
-        
+
+    def get(
+        self, confounds: Optional[pd.DataFrame] = None
+    ) -> Tuple[np.ndarray, List[str]]:
         column_names = []
         basis = ["sine", "cosine"]
         regressors = np.zeros((self.n_tps, len(basis) * len(self.search_frequencies)))
@@ -44,9 +49,10 @@ class FrequencyRegressors(DesignMatrixRegressors):
 
         return (regressors, column_names)
 
+
 class DriftRegressors(DesignMatrixRegressors):
     def __init__(
-        self, 
+        self,
         time_points: Tuple[float, float],
         high_pass_threshold: float = 0.01,
         add_constant: bool = True,
@@ -57,18 +63,21 @@ class DriftRegressors(DesignMatrixRegressors):
         self.high_pass_threshold = high_pass_threshold
         self.add_constant = add_constant
         self.n_tps = len(self.time_points)
-    
-    def get(self, confounds: Optional[pd.DataFrame] = None) -> Tuple[np.ndarray, List[str]]:
-    
+
+    def get(
+        self, confounds: Optional[pd.DataFrame] = None
+    ) -> Tuple[np.ndarray, List[str]]:
         n_times = np.arange(self.n_tps)
         dt = (self.time_points[-1] - self.time_points[0]) / (self.n_tps - 1)
         if self.high_pass_threshold * dt >= 0.5:
-            warn(f"""
+            warn(
+                f"""
             High-pass filter will span all accessible frequencies
             and saturate the design matrix. You may want to reduce
             the high_pass _threshold value. 
             The provided value is {self.high_pass_threshold} Hz.
-            """)
+            """
+            )
         order = np.minimum(
             self.n_tps - 1,
             int(np.floor(2 * self.n_tps * self.high_pass_threshold * dt)),
@@ -89,13 +98,14 @@ class DriftRegressors(DesignMatrixRegressors):
             cosine_drift[:, -1] = 1
             return (cosine_drift, column_names + ["constant"])
         else:
-            return (cosine_drift[:,:-1], column_names)
-    
+            return (cosine_drift[:, :-1], column_names)
+
+
 class MotionParameters(DesignMatrixRegressors):
     def __init__(
         self,
         time_indices: Tuple[int, int],
-        mc_params: Literal[6,24],
+        mc_params: Literal[6, 24],
     ):
         super().__init__()
         self.confounds_required = True
@@ -103,7 +113,6 @@ class MotionParameters(DesignMatrixRegressors):
         self.mc_params = mc_params
 
     def get(self, confounds: Optional[pd.DataFrame] = None):
-
         assert confounds is not None, f"`confounds` must be specified to use this class"
         col_names = self._get_column_names()
         idx_1, idx_2 = self.time_indices[0], self.time_indices[1] + 1
@@ -111,19 +120,24 @@ class MotionParameters(DesignMatrixRegressors):
         return (regressors, col_names)
 
     def _get_column_names(self):
-        
-        MOTION_BASE: List[str] = ['trans', 'rot']
-        MOTION_DIR: List[str] = ['x', 'y', 'z']
-        MOTION_SUFFIX: List[str] = ['derivative1','power2','derivative1_power2']
+        MOTION_BASE: List[str] = ["trans", "rot"]
+        MOTION_DIR: List[str] = ["x", "y", "z"]
+        MOTION_SUFFIX: List[str] = ["derivative1", "power2", "derivative1_power2"]
 
-        regressor_labels = ['_'.join(list(i)) for i in itertools.product(MOTION_BASE, MOTION_DIR)]
+        regressor_labels = [
+            "_".join(list(i)) for i in itertools.product(MOTION_BASE, MOTION_DIR)
+        ]
         if self.mc_params == 6:
             return regressor_labels
         elif self.mc_params == 24:
-            return regressor_labels + ['_'.join(list(i)) for i in itertools.product(MOTION_BASE, MOTION_DIR, MOTION_SUFFIX)]
+            return regressor_labels + [
+                "_".join(list(i))
+                for i in itertools.product(MOTION_BASE, MOTION_DIR, MOTION_SUFFIX)
+            ]
         else:
             raise ValueError("`self.mc_params` must be set to 6 or 24")
-    
+
+
 class DesignMatrix:
     def __init__(
         self,
@@ -150,7 +164,6 @@ class DesignMatrix:
         self.column_names = []
 
     def add_regressor(self, regressors: DesignMatrixRegressors) -> None:
-        
         if regressors.confounds_required:
             confounds_df = self._get_confounds(self.bold_path, self.confounds_tsv)
             regressors_array, regressor_labels = regressors.get(confounds_df)
@@ -162,15 +175,15 @@ class DesignMatrix:
         self.column_names += regressor_labels
 
     def build_design_matrix(self) -> pd.DataFrame:
-        
-        assert self.design_matrix.shape[-1] > 0, f"No regressors have been added to the design matrix yet"
+        assert (
+            self.design_matrix.shape[-1] > 0
+        ), f"No regressors have been added to the design matrix yet"
 
         return pd.DataFrame(
             self.design_matrix, index=self.time_points, columns=self.column_names
         )
 
     def plot_design_matrix(self, figsize=(4, 8)):
-
         from nilearn.plotting import plot_design_matrix
 
         fig, ax = plt.subplots(figsize=figsize)
@@ -216,49 +229,53 @@ class DesignMatrix:
         return time_points
 
     def _get_confounds(
-        self, 
+        self,
         bold_path: Union[str, Path, None] = None,
         confounds_tsv: Union[str, Path, pd.DataFrame, None] = None,
-        ) -> pd.DataFrame:
-
+    ) -> pd.DataFrame:
         if isinstance(confounds_tsv, pd.DataFrame):
             return confounds_tsv
-        
+
         elif confounds_tsv is None:
             # Check if a tsv can be found based on oscprep heuristics
-            assert bold_path is not None, f"Confounds file cannot be found without a `bold_path` input"
-            confounds_tsv = self._get_confounds_path(bold_path, 'tsv')
-            return pd.read_csv(confounds_tsv, sep='\t')
-        
+            assert (
+                bold_path is not None
+            ), f"Confounds file cannot be found without a `bold_path` input"
+            confounds_tsv = self._get_confounds_path(bold_path, "tsv")
+            return pd.read_csv(confounds_tsv, sep="\t")
+
         else:
             confounds_tsv = Path(confounds_tsv)
             if not confounds_tsv.exists():
                 raise FileNotFoundError(
                     f"Error: file path does not exist [{confounds_tsv}]"
                 )
-            return pd.read_csv(confounds_tsv, sep='\t')
+            return pd.read_csv(confounds_tsv, sep="\t")
 
     def _get_confounds_path(
-        self, 
-        bold_path: Union[str,Path],
-        desc: Literal['tsv', 'metadata']
-        ) -> Path:
-
+        self, bold_path: Union[str, Path], desc: Literal["tsv", "metadata"]
+    ) -> Path:
         preproc_bold_suffix = "space-T1w_desc-preproc_bold.nii.gz"
         confounds_suffix = "desc-confounds_timeseries.tsv"
         confounds_metadata_suffix = "desc-confounds_timeseries.json"
 
         bold_path = Path(bold_path).resolve()
-        confounds_tsv = Path(str(bold_path).replace(preproc_bold_suffix, confounds_suffix))
-        confounds_metadata = Path(str(bold_path).replace(preproc_bold_suffix, confounds_metadata_suffix))
+        confounds_tsv = Path(
+            str(bold_path).replace(preproc_bold_suffix, confounds_suffix)
+        )
+        confounds_metadata = Path(
+            str(bold_path).replace(preproc_bold_suffix, confounds_metadata_suffix)
+        )
 
         for p in [bold_path, confounds_tsv, confounds_metadata]:
             if not p.exists():
-                raise FileNotFoundError(f"Error: file path does not exist [{bold_path}]")
-        
-        if desc == 'tsv':
+                raise FileNotFoundError(
+                    f"Error: file path does not exist [{bold_path}]"
+                )
+
+        if desc == "tsv":
             return confounds_tsv
-        elif desc == 'metadata':
+        elif desc == "metadata":
             return confounds_metadata
         else:
             raise ValueError("`desc` must be set to 'tsv' or 'metadata'")
--- fastfmri_toolbox/visualize/base.py
+++ fastfmri_toolbox/visualize/base.py
@@ -7,6 +7,7 @@ import matplotlib.cm as cm
 import numpy as np
 import nibabel as nib
 
+
 class PlotSlices:
     def __init__(
         self,
@@ -18,18 +19,17 @@ class PlotSlices:
         self.base_data = self._load_base_nifti(base_nifti_path)
         self.base_z_indices = self._get_z_indices(self.base_data, z_slice_range)
 
-    def plot_base(self, cmap: str = 'gray', base_vmax: Union[float, None] = None):
-        
+    def plot_base(self, cmap: str = "gray", base_vmax: Union[float, None] = None):
         x_max, y_max, z_max = self.base_data.shape
         self.num_slices: int = len(self.base_z_indices)
-        self.num_rows: int = (self.num_slices + (self.num_cols-1))  // self.num_cols
+        self.num_rows: int = (self.num_slices + (self.num_cols - 1)) // self.num_cols
 
         self.fig, self.axes = plt.subplots(
-            self.num_rows, 
-            self.num_cols, 
-            figsize=(self.num_cols * 2.4, self.num_rows * 2.4)
+            self.num_rows,
+            self.num_cols,
+            figsize=(self.num_cols * 2.4, self.num_rows * 2.4),
         )
-        
+
         if base_vmax is None:
             base_vmax = self.base_data.max()
 
@@ -38,76 +38,80 @@ class PlotSlices:
             col = i % self.num_cols
             if self.num_rows == 1:
                 self.axes[col].imshow(
-                    self.base_data[:,:,z_slice],
-                    cmap = cmap,
-                    vmax = base_vmax,
-                    zorder = 1,
+                    self.base_data[:, :, z_slice],
+                    cmap=cmap,
+                    vmax=base_vmax,
+                    zorder=1,
                 )
                 self.axes[col].text(
-                    int(.02*x_max),
-                    int(.1*y_max),
-                    f"z={z_slice}",
-                    c='white'
+                    int(0.02 * x_max), int(0.1 * y_max), f"z={z_slice}", c="white"
                 )
             else:
                 self.axes[row, col].imshow(
-                    self.base_data[:,:,z_slice],
-                    cmap = 'gray',
-                    vmax = base_vmax,
-                    zorder = 1,
+                    self.base_data[:, :, z_slice],
+                    cmap="gray",
+                    vmax=base_vmax,
+                    zorder=1,
                 )
                 self.axes[row, col].text(
-                    int(.02*x_max),
-                    int(.15*y_max),
-                    f"z={z_slice}",
-                    c='white'
+                    int(0.02 * x_max), int(0.15 * y_max), f"z={z_slice}", c="white"
                 )
 
         # Hide empty subplots if there are fewer than `self.num_cols` images in the last row
         if self.num_slices % self.num_cols != 0:
             for j in range(self.num_slices % self.num_cols, self.num_cols):
                 if self.num_rows == 1:
-                    self.axes[j].axis('off')
+                    self.axes[j].axis("off")
                 else:
-                    self.axes[self.num_rows - 1, j].axis('off')
+                    self.axes[self.num_rows - 1, j].axis("off")
 
         plt.subplots_adjust(wspace=0.05, hspace=0.05)
         self._turn_off_axis()
-    
+
     def add_contours(
-        self, 
-        mask_nifti_path: Union[str, Path], 
-        c: str = 'cyan',
-        lw: float = .5,
+        self,
+        mask_nifti_path: Union[str, Path],
+        c: str = "cyan",
+        lw: float = 0.5,
         zorder: int = 2,
     ):
-
         from skimage import measure
 
         mask_data = self._validate_nifti(mask_nifti_path)
-        mask_data[mask_data != 0] = 1 # Binarize all values not equal to zero
-        
+        mask_data[mask_data != 0] = 1  # Binarize all values not equal to zero
+
         for i, z_slice in enumerate(self.base_z_indices):
             row = i // self.num_cols
             col = i % self.num_cols
-            contours = measure.find_contours(mask_data[:,:,z_slice], 0.5)
+            contours = measure.find_contours(mask_data[:, :, z_slice], 0.5)
             if self.num_rows == 1:
                 for contour in contours:
-                    self.axes[col].plot(contour[:,1],contour[:,0],linewidth=lw, color=c, zorder=zorder)
+                    self.axes[col].plot(
+                        contour[:, 1],
+                        contour[:, 0],
+                        linewidth=lw,
+                        color=c,
+                        zorder=zorder,
+                    )
             else:
                 for contour in contours:
-                    self.axes[row, col].plot(contour[:,1],contour[:,0],linewidth=lw, color=c, zorder=zorder)
+                    self.axes[row, col].plot(
+                        contour[:, 1],
+                        contour[:, 0],
+                        linewidth=lw,
+                        color=c,
+                        zorder=zorder,
+                    )
 
     def add_overlay(
         self,
         overlay_nifti: Union[str, Path, nib.Nifti1Image],
         threshold: float,
-        cmap: str = 'magma',
+        cmap: str = "magma",
         alpha: float = 1.0,
         vmax: Union[float, None] = None,
         zorder: int = 3,
     ):
-        
         data = self._validate_nifti(overlay_nifti)
         if vmax is None:
             vmax = data.max()
@@ -115,45 +119,56 @@ class PlotSlices:
         for i, z_slice in enumerate(self.base_z_indices):
             row = i // self.num_cols
             col = i % self.num_cols
-            slice_data = data[:,:,z_slice]
+            slice_data = data[:, :, z_slice]
             masked_slice_data = np.ma.masked_where(slice_data <= threshold, slice_data)
             if self.num_rows == 1:
-                self.axes[col].imshow(masked_slice_data, cmap=cmap, alpha=alpha, interpolation='none', zorder=zorder, vmax=vmax)
+                self.axes[col].imshow(
+                    masked_slice_data,
+                    cmap=cmap,
+                    alpha=alpha,
+                    interpolation="none",
+                    zorder=zorder,
+                    vmax=vmax,
+                )
             else:
-                self.axes[row, col].imshow(masked_slice_data, cmap=cmap, alpha=alpha, interpolation='none', zorder=zorder, vmax=vmax)
+                self.axes[row, col].imshow(
+                    masked_slice_data,
+                    cmap=cmap,
+                    alpha=alpha,
+                    interpolation="none",
+                    zorder=zorder,
+                    vmax=vmax,
+                )
 
     def _turn_off_axis(self):
-
         for ax in self.axes.flatten():
             ax.set_xticks([])
             ax.set_xticklabels([])
             ax.set_yticks([])
             ax.set_yticklabels([])
-    
+
     def _load_base_nifti(self, nifti: Union[str, Path, nib.Nifti1Image]) -> np.ndarray:
         """
         Convert str, Path, or nib.Nifti1Image into numpy array.
         Numpy array is generated for the base image.
         """
-        
+
         if isinstance(nifti, nib.Nifti1Image):
             data = nifti.get_fdata()
         else:
             nifti = Path(nifti).resolve()
             assert nifti.name.endswith("nii.gz"), f"{nifti.name} must end with [nii.gz]"
             if not nifti.exists():
-                raise FileNotFoundError(
-                    f"Error: file path does not exist [{nifti}]"
-                )
+                raise FileNotFoundError(f"Error: file path does not exist [{nifti}]")
             data = nib.load(nifti).get_fdata()
-        
+
         # Image must be 3D or 4D
         n_dims = len(data.shape)
-        assert n_dims in [3,4], f"{n_dims} must be 3 or 4"
+        assert n_dims in [3, 4], f"{n_dims} must be 3 or 4"
 
         # If 4D, the base image is the temporal mean image
         return data if len(data.shape) == 3 else np.mean(data, axis=-1)
-    
+
     def _validate_nifti(self, nifti: Union[str, Path, nib.Nifti1Image]) -> np.ndarray:
         """
         Convert str, Path, or nib.Nifti1Image into numpy array.
@@ -164,18 +179,24 @@ class PlotSlices:
         else:
             data = nib.load(nifti).get_fdata()
 
-        assert data.shape == self.base_data.shape, f"Dimensions does not match with base image [{self.base_data.shape}]"
+        assert (
+            data.shape == self.base_data.shape
+        ), f"Dimensions does not match with base image [{self.base_data.shape}]"
 
         return data
-    
-    def _get_z_indices(self, nifti_data: np.ndarray, z_slice_range: Union[None, Tuple[int, int]]) -> List[int]:
+
+    def _get_z_indices(
+        self, nifti_data: np.ndarray, z_slice_range: Union[None, Tuple[int, int]]
+    ) -> List[int]:
         """
         Return list of indices corresponding to z-slices that are not empty
         """
         num_slices = nifti_data.shape[-1]
-        z_slices = [i for i in range(num_slices) if not np.all(np.equal(nifti_data[:,:,i],0))]
+        z_slices = [
+            i for i in range(num_slices) if not np.all(np.equal(nifti_data[:, :, i], 0))
+        ]
         if z_slice_range is None:
             return z_slices
         else:
             assert z_slice_range[1] > z_slice_range[0]
-            return z_slices[z_slice_range[0]:z_slice_range[1]]
+            return z_slices[z_slice_range[0] : z_slice_range[1]]
